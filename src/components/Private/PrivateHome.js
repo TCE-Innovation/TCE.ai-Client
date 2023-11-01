6598dce94920faaff503dd32b6e230a734825f56
matthewbayne <mbayne@tcelect.net>
2023-11-01 15:45:33 -0400

    center text in cards

--- src/components/Private/PrivateHome.js
+++ src/components/Private/PrivateHome.js
@@ -27,7 +27,7 @@ const PrivateHome = () => {
             <Box my={4}>
 
                 <Typography variant="h4" component="h1" gutterBottom>
-                    Welcome to TCE.ai
+                    Welcome to TCIG.nyc
                 </Typography>
 
                     <Typography paragraph>
