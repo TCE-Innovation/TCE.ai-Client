73149565c4f6ef96de10a7b9990ed20daa1f72d7
matthewbayne <mbayne@tcelect.net>
2023-11-07 11:19:52 -0500

    added links to documents in private tech partner

--- src/components/PrivateFeatures/TechPartners.js
+++ src/components/PrivateFeatures/TechPartners.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import "./style.css";
+import "./privateStyle.css";
 
 import ProCore from "../../img/PartnerImages/ProCore.png";
 import Airtable from "../../img/PartnerImages/Airtable.png";
@@ -12,180 +12,135 @@ import IconButton from '@mui/material/IconButton';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
+const partnerData = [
+  {
+    name: "Airtable",
+    imgSrc: Airtable,
+    description: "A host for multiple internal applications for use across a wide range of workflows within TCE as a whole.",
+    extraText: "Airtable assists various user groups and their specific needs, including TCIG, Estimating, Safety, Engineers, and Project Managers. TCIG and Estimating leverage Airtable's static database functionality to track internal assets and plan future bids, complementing their workflow with Bridgit Bench. The efficiency gains are further enhanced through Airtable's process logic and email automation, seamlessly integrated into multiple applications. The safety team benefits from automation in capturing track/safety certifications in a database and notifies safety managers of expiration dates, reducing downtime required for updating certifications which often have lead times of over three months.",
+    contactName: "Chris Kruger",
+    contactEmail: "ckruger@tcelect.net",
+    website: 'https://www.airtable.com',
+    documents: [
+      {title: "GO Tracker White Paper", url: "https://judlauent.sharepoint.com/:b:/s/TCEInnovation/ETEIqNwZPvFPsCP6KLnLxYIBjt_3LmFY_SAtRvYhBchi_Q?e=GgEatL"},
+      {title: "GO Tracker SOP", url: "https://judlauent.sharepoint.com/:b:/s/TCEInnovation/EehYuVJdG9NJj_8Hp4_gqskBg8G5xFP0W6s7iQKaslYRDA?e=LL6t1E"}
+    ]
+  },
+  {
+    name: "ProCore",
+    imgSrc: ProCore,
+    description: "Work more efficiently, communicate better, and build faster from a single source of truth.",
+    extraText: "Procore streamlines project management by centralizing document storage and eliminating the hassles of fragmented version control. This all-in-one platform enhances the workflows of all project personnel, whether they're in the field or in the office. Preconstruction tasks like estimate management, bid coordination, budget calculations, and team personnel tracking become effortless. During active projects, Procore excels at automating RFIs and Submittals, simplifying mark-ups and drawing labeling, maintaining comprehensive daily logs in real-time, providing fillable Quality and Safety reports, and offering numerous other invaluable features.",
+    contactName: "Patrick Besser",
+    contactEmail: "pbesser@tcelect.net",
+    website: 'https://www.procore.com',
+    documents: []
+  },
+  {
+    name: "OpenSpace",
+    imgSrc: OpenSpace,
+    description: "Visual documentation of construction sites from walk-throughs.",
+    extraText: "OpenSpace offers engineers and project managers a practical solution for progress tracking, featuring a 360-degree camera attached to a hard hat that automatically records construction sites in 3-D. Executive Leadership can now remotely view the walk through of a project, in a similar manner to Google Maps, without the need to be there in-person. It allows for the addition of detailed photos and field notes during site walkthroughs, providing a comprehensive view for remote oversight, timeline tracking, and cost-effective insurance management.",
+    contactName: "Rory O'Neill",
+    contactEmail: "roneill@tcelect.net",
+    website: 'https://www.openspace.ai',
+    documents: [
+      {title: "OpenSpace SOP", url: "https://judlauent.sharepoint.com/:b:/s/TCEInnovation/ESGFy6NJP6pHteZx0vyLLygBb7XFxuGv4EnzGyuA5WOOQQ?e=GZQcte"},
+    ]
+  },
+  {
+    name: "Bridgit",
+    imgSrc: Bridgit,
+    description: "Transforms workforce data into actionable insights that inform an organization’s strategic and tactical business decisions.",
+    extraText: "Bridgit Bench improves project management by seamlessly integrating project and individual employee data, enabling more precise timelines and workforce optimization. During the pre-construction phase, it facilitates the creation and assignment of role titles and streamlines the coordination of Key Personnel for Design-Build Projects. As projects transition into the active phase and employees are assigned to specific roles, Bridgit Bench ensures optimal allocation, preventing overwork, and strategically placing employees based on their qualifications, certifications, and project history to meet both their needs and the project's requirements.",
+    contactName: "Jacob Shavel",
+    contactEmail: "jshavel@tcelect.net",
+    website: 'https://www.gobridgit.com',
+    documents: [
+      {title: "Bridgit Bench White Paper", url: "https://judlauent.sharepoint.com/:b:/s/TCEInnovation/EQyO1dDTWFxDjvk9Gr5tN5YBQqF6b73gNlDd3jGcNjQ9Yg?e=Zp6Ply"},
+    ]
+  },
+  {
+    name: "EZOfficeInventory",
+    imgSrc: EZ,
+    description: "Asset management for maximizing overstock usage and minimizing item surplus.",
+    extraText: "EZOffice Inventory provides comprehensive asset tracking capabilities for items that can be allocated to various project sites. It mitigates the risk of lost or misused assets while optimizing resource utilization. Warehouse users benefit from streamlined processes for efficiently managing the inflow and outflow of items. Notable features, including location tracking and customized item groupings, eliminate the need for manual searches, ensuring zero downtime and efficient access to available in-stock items.",
+    contactName: "Chris Kruger",
+    contactEmail: "ckruger@tcelect.net",
+    website: 'https://ezo.io/ezofficeinventory/',
+    documents: []
+  },
+  {
+    name: "FieldWire",
+    imgSrc: FieldWire,
+    description: "Standardized workflow and collaboration for marking up drawings.",
+    extraText: "Fieldwire streamlines collaboration and version control for multiple engineers, eliminating the need to constantly ensure that work is done on the latest document version. In the past, projects faced challenges with communication among engineers and maintaining consistency in project drawings. However, Fieldwire offers a standardized space for document history and collaborative drawing markups, effectively reducing the pain points associated with maintaining consistency, enhancing communication, and improving overall efficiency.",
+    contactName: "Rory O'Neill",
+    contactEmail: "roneill@tcelect.net",
+    website: 'https://www.fieldwire.com',
+    documents: []
+  }
+];
+
 const TechPartners = () => {
-  const [readMoreAirtable, setReadMoreAirtable] = useState(false);
-  const [readMoreProcore, setReadMoreProcore] = useState(false);
-  const [readMoreBridgit, setReadMoreBridgit] = useState(false);
-  const [readMoreFieldwire, setReadMoreFieldwire] = useState(false);
-  const [readMoreOpenSpace, setReadMoreOpenSpace] = useState(false);
-  const [readMoreEZ, setReadMoreEZ] = useState(false);
+  const [readMoreStates, setReadMoreStates] = useState(new Array(partnerData.length).fill(false));
 
-  const handleReadMoreAirtableClick = () => {
-    setReadMoreAirtable(!readMoreAirtable); 
+  const toggleReadMore = (index) => {
+    const newReadMoreStates = readMoreStates.map((state, i) => (i === index ? !state : state));
+    setReadMoreStates(newReadMoreStates);
   };
-  const handleReadMoreProcoreClick = () => {
-    setReadMoreProcore(!readMoreProcore); 
-  }
-  const handleReadMoreBridgitClick = () => {
-    setReadMoreBridgit(!readMoreBridgit); 
-  }
-  const handleReadMoreFieldwireClick = () => {
-    setReadMoreFieldwire(!readMoreFieldwire); 
-  }
-  const handleReadMoreOpenSpaceClick = () => {
-    setReadMoreOpenSpace(!readMoreOpenSpace); 
-  }
-  const handleReadMoreEZClick = () => {
-    setReadMoreEZ(!readMoreEZ);
-  }
-
 
   return (
     <div className="container">
-        <div className="header">TECH PARTNERS</div>
-
-        <br />
-        <br />
-
-        <div className="cards-container">
-
-          <div className="card">
-            <div className="card-image">
-              <img src={Airtable} alt="Airtable" onClick={() => window.open('https://www.airtable.com', '_blank')}></img>
-            </div>
-
-            <div className="card-body">
-
-              <p className="card-text">A host for multiple internal applications for use across a wide range of workflows within TCE as a whole.</p>
-              {readMoreAirtable && (
-                <>
-                  <p className="extra-text">
-                    Airtable assists various user groups and their specific needs, including TCIG, Estimating, Safety, Engineers, and Project Managers. TCIG and Estimating leverage Airtable's static database functionality to track internal assets and plan future bids, complementing their workflow with Bridgit Bench. The efficiency gains are further enhanced through Airtable's process logic and email automation, seamlessly integrated into multiple applications. The safety team benefits from automation in capturing track/safety certifications in a database and notifies safety managers of expiration dates, reducing downtime required for updating certifications which often have lead times of over three months. 
-                  </p>
-                  <p className="contact-text">
-                    Point of Contact: Chris Kruger <a href="mailto:ckruger@tcelect.net">ckruger@tcelect.net</a>
-                  </p>
-                </>
-              )}         
-            </div>
-            <IconButton onClick={handleReadMoreAirtableClick} className="read-more-button" disableRipple>
-                {readMoreAirtable ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
-            </IconButton> 
-          </div>
-
-          <div className="card">
-            <div className="card-image">
-              <img src={ProCore} alt="ProCore" onClick={() => window.open('https://www.procore.com', '_blank')} style={{ cursor: 'pointer' }}></img>
-            </div>
-            <div className="card-body">
-              <p className="card-text">Work more efficiently, communicate better, and build faster from a single source of truth.</p>
-              {readMoreProcore && (
-                <>
-                  <p className="extra-text">
-                    Procore streamlines project management by centralizing document storage and eliminating the hassles of fragmented version control. This all-in-one platform enhances the workflows of all project personnel, whether they're in the field or in the office. Preconstruction tasks like estimate management, bid coordination, budget calculations, and team personnel tracking become effortless. During active projects, Procore excels at automating RFIs and Submittals, simplifying mark-ups and drawing labeling, maintaining comprehensive daily logs in real-time, providing fillable Quality and Safety reports, and offering numerous other invaluable features.
-                  </p>
-                  <p className="contact-text">
-                    Point of Contact: Patrick Besser <a href="mailto:pbesser@tcelect.net">pbesser@tcelect.net</a>
-                  </p>
-                </>
-              )}    
-            </div>
-            <IconButton onClick={handleReadMoreProcoreClick} className="read-more-button" disableRipple>
-                {readMoreProcore ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
-            </IconButton>        
-          </div>
-
-          <div className="card">
-          <div className="card-image">
-            <img src={OpenSpace} alt="OpenSpace" onClick={() => window.open('https://www.openspace.ai', '_blank')} style={{ cursor: 'pointer' }}></img>
-          </div>
-            <div className="card-body">
-              <p className="card-text">Visual documentation of construction sites from walk-throughs.</p>
-              {readMoreOpenSpace && (
-                <>
-                  <p className="extra-text">
-                    OpenSpace offers engineers and project managers a practical solution for progress tracking, featuring a 360-degree camera attached to a hard hat that automatically records construction sites in 3-D. Executive Leadership can now remotely view the walk through of a project, in a similar manner to Google Maps, without the need to be there in-person. It allows for the addition of detailed photos and field notes during site walkthroughs, providing a comprehensive view for remote oversight, timeline tracking, and cost-effective insurance management.
-                  </p>
-                  <p className="contact-text">
-                    Point of Contact: Rory O'Neill <a href="mailto:roneill@tcelect.net">roneill@tcelect.net</a>
-                  </p>
-                </>
-              )}  
-            </div>
-            <IconButton onClick={handleReadMoreOpenSpaceClick} className="read-more-button" disableRipple>
-                {readMoreOpenSpace ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
-            </IconButton>       
-          </div>
+      <div className="header">TECH PARTNERS</div>
+      <br /><br />
+      <div className="cards-container">
+        {partnerData.map((partner, index) => (
+          <div className="card" key={index}>
+            
+            <div className="one-liner-container">
+              <div className="card-image">
+                <img
+                  src={partner.imgSrc}
+                  alt={partner.name}
+                  onClick={() => window.open(partner.website, '_blank')}
+                  style={{ cursor: 'pointer' }}
+                />
+              </div>
   
-          <div className="card">
-          <div className="card-image">
-            <img src={Bridgit} alt="Bridgit" onClick={() => window.open('https://www.gobridgit.com', '_blank')} style={{ cursor: 'pointer' }}></img>
-          </div>
-            <div className="card-body">
-              <p className="card-text">Transforms workforce data into actionable insights that inform an organization’s strategic and tactical business decisions.</p>
-              {readMoreBridgit && (
-                <>
-                  <p className="extra-text">
-                    Bridgit Bench improves project management by seamlessly integrating project and individual employee data, enabling more precise timelines and workforce optimization. During the pre-construction phase, it facilitates the creation and assignment of role titles and streamlines the coordination of Key Personnel for Design-Build Projects. As projects transition into the active phase and employees are assigned to specific roles, Bridgit Bench ensures optimal allocation, preventing overwork, and strategically placing employees based on their qualifications, certifications, and project history to meet both their needs and the project's requirements.
-                  </p>
-                  <p className="contact-text">
-                    Point of Contact: Jacob Shavel <a href="mailto:jshavel@tcelect.net">jshavel@tcelect.net</a>
-                  </p>
-                </>
-              )}           
-            </div>
-            <IconButton onClick={handleReadMoreBridgitClick} className="read-more-button" disableRipple>
-                {readMoreBridgit ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
-            </IconButton> 
-          </div>
+              <div className="card-body">
+                <p className="card-text">{partner.description}</p>
+              </div>    
 
-          <div className="card">
-          <div className="card-image">
-            <img src={EZ} alt="EZOffice" onClick={() => window.open('https://ezo.io/ezofficeinventory/', '_blank')} style={{ cursor: 'pointer' }}></img>
-          </div>
-            <div className="card-body">
-              <p className="card-text">Asset management for maximizing overstock usage and minimizing item surplus.</p>
-              {readMoreEZ && (
-                <>
-                  <p className="extra-text">
-                    EZOffice Inventory provides comprehensive asset tracking capabilities for items that can be allocated to various project sites. It mitigates the risk of lost or misused assets while optimizing resource utilization. Warehouse users benefit from streamlined processes for efficiently managing the inflow and outflow of items. Notable features, including location tracking and customized item groupings, eliminate the need for manual searches, ensuring zero downtime and efficient access to available in-stock items.
-                  </p>
-                  <p className="contact-text">
-                    Point of Contact: Chris Kruger <a href="mailto:ckruger@tcelect.net">ckruger@tcelect.net</a>
-                  </p>
-                </>
-              )}    
+              <IconButton onClick={() => toggleReadMore(index)} className="read-more-button" disableRipple>
+                {readMoreStates[index] ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
+              </IconButton>          
             </div>
-            <IconButton onClick={handleReadMoreEZClick} className="read-more-button" disableRipple>
-                {readMoreEZ ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
-            </IconButton>    
-          </div>
-
-          <div className="card">
-          <div className="card-image">
-            <img src={FieldWire} alt="FieldWire" onClick={() => window.open('https://www.fieldwire.com', '_blank')} style={{ cursor: 'pointer' }}></img>
-          </div>
-            <div className="card-body">
-              <p className="card-text">Standardized workflow and collaboration for marking up drawings.</p>
-              {readMoreFieldwire && (
-                <>
-                  <p className="extra-text">
-                    Fieldwire streamlines collaboration and version control for multiple engineers, eliminating the need to constantly ensure that work is done on the latest document version. In the past, projects faced challenges with communication among engineers and maintaining consistency in project drawings. However, Fieldwire offers a standardized space for document history and collaborative drawing markups, effectively reducing the pain points associated with maintaining consistency, enhancing communication, and improving overall efficiency.
-                  </p>
+  
+            {readMoreStates[index] && (
+              <div className="expanded-container">
+                <div className="link-box">
+                  {partner.documents && partner.documents.map((doc, docIndex) => (
+                    <div key={docIndex}>
+                      <a href={doc.url} target="_blank" rel="noopener noreferrer" className="link-text">{doc.title}</a>
+                    </div>
+                  ))}
+                </div>
+                <div className="expanded-card-body">
+                  <p className="extra-text">{partner.extraText}</p>
                   <p className="contact-text">
-                    Point of Contact: Rory O'Neill <a href="mailto:roneill@tcelect.net">roneill@tcelect.net</a>
+                    Point of Contact: {partner.contactName} <a href={`mailto:${partner.contactEmail}`}>{partner.contactEmail}</a>
                   </p>
-                </>
-              )}     
-            </div>
-            <IconButton onClick={handleReadMoreFieldwireClick} className="read-more-button" disableRipple>
-                {readMoreFieldwire ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
-            </IconButton>   
+                </div>
+              </div>
+            )}
+  
           </div>
-
-        </div>
+        ))}
+      </div>
     </div>
   );
+  
 };
 
 export default TechPartners;
