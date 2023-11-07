aeff940f9fd907d6a0152cf40a1192d183f39c8e
RoryMO <120664694+RoryMO@users.noreply.github.com>
2023-11-07 11:58:13 -0500

    Radio Buttons for Run Type

--- src/components/PrivateFeatures/CRO.js
+++ src/components/PrivateFeatures/CRO.js
@@ -7,9 +7,16 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import CircularProgress from '@mui/material/CircularProgress';
+// Radio buttons for selecting run type
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 //DEPENDENCIES
 import axios from 'axios';
+// import RunTypeRadioButtons from './RunTypeRadioButtons'; // Import the radio buttons component
 
 const CRO = () => {
     const [pullsheet, setPullsheet] = useState('');
@@ -141,6 +148,21 @@ const CRO = () => {
                             {showCableSizeSheet ? 'Use Standard Cable Sizes Sheet' : 'OPTIONAL: Upload Your Cable Sizes'}
                     </Button>
 
+                    <div style={{ margin: '40px 0' }}></div>
+
+                    <FormControl>
+                        <FormLabel id="demo-row-radio-buttons-group-label">Select Run Type</FormLabel>
+                        <RadioGroup
+                            row
+                            aria-labelledby="demo-row-radio-buttons-group-label"
+                            name="row-radio-buttons-group"
+                        >
+                            <FormControlLabel value="Conduit" control={<Radio />} label="Conduit" />
+                            <FormControlLabel value="Messenger Bundle" control={<Radio />} label="Messenger Bundle" />
+                            <FormControlLabel value="Cable Tray" disabled control={<Radio />} label="Cable Tray" />
+                        </RadioGroup>
+                    </FormControl>
+
                     <Box sx={{ marginTop: 4}}>
                         <Button
                             variant="contained"
@@ -187,3 +209,134 @@ const CRO = () => {
 };
 
 export default CRO;
+
+
+// //REACT
+// import React, { useState } from 'react';
+// import { Input } from 'reactstrap';
+
+// //MUI
+// import Box from '@mui/material/Box';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+// import CircularProgress from '@mui/material/CircularProgress';
+
+// //DEPENDENCIES
+// import axios from 'axios';
+
+// // Move the RunTypeRadioButtons import here from the previous import section
+// import Radio from '@mui/material/Radio';
+// import RadioGroup from '@mui/material/RadioGroup';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import FormControl from '@mui/material/FormControl';
+// import FormLabel from '@mui/material/FormLabel';
+
+// // Define the RunTypeRadioButtons component
+// const RunTypeRadioButtons = ({ selectedRunType, setSelectedRunType }) => {
+//   return (
+//     <FormControl component="fieldset">
+//       <FormLabel id="run-type-label">Select Run Type</FormLabel>
+//       <RadioGroup
+//         aria-labelledby="run-type-label"
+//         name="run-type"
+//         value={selectedRunType}
+//         onChange={(e) => setSelectedRunType(e.target.value)}
+//       >
+//         <FormControlLabel
+//           value="Conduit"
+//           control={<Radio />}
+//           label="Conduit"
+//         />
+//         <FormControlLabel
+//           value="Messenger Bundle"
+//           control={<Radio />}
+//           label="Messenger Bundle"
+//         />
+//         <FormControlLabel
+//           value="Cable Tray"
+//           control={<Radio />}
+//           label="Cable Tray"
+//           disabled
+//         />
+//       </RadioGroup>
+//     </FormControl>
+//   );
+// };
+
+// const CRO = () => {
+//   const [pullsheet, setPullsheet] = useState('');
+//   const [cableSizes, setCableSizes] = useState('');
+//   const [response, setResponse] = useState('');
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState('');
+//   const [showCableSizeSheet, setShowCableSizeSheet] = useState(false);
+
+//   // Add a state variable to manage the selected run type
+//   const [selectedRunType, setSelectedRunType] = useState('Conduit');
+
+//   const cro = async () => {
+//     // ... (rest of your code)
+//   };
+
+//   return (
+//     <Box
+//       sx={{
+//         // ... (styles for the outer box)
+//       }}
+//     >
+//       <Box
+//         sx={{
+//           // ... (styles for the inner box)
+//         }}
+//       >
+//         <Typography variant="body2" fontSize="20px">
+//           {/* ... (your text content) */}
+//         </Typography>
+
+//         <label style={{ fontSize: '20px', marginBottom: '10px' }}>
+//           Upload Pull Sheet
+//         </label>
+//         <Input
+//           type="file"
+//           id="pullsheetInput"
+//           accept=".xlsx, .xls"
+//           onChange={(e) => setPullsheet(e.target.files[0])}
+//         />
+
+//         <RunTypeRadioButtons
+//           selectedRunType={selectedRunType}
+//           setSelectedRunType={setSelectedRunType}
+//         />
+
+//         {/* ... (rest of your code) */}
+//       </Box>
+//       <Box
+//         sx={{
+//           // ... (styles for the inner box)
+//         }}
+//       >
+//         {loading ? (
+//           <>
+//             <CircularProgress />
+//             <Typography variant="body2" mt={2}>
+//               Optimizing...
+//             </Typography>
+//           </>
+//         ) : response && (
+//           <>
+//             <a href={response} target="_blank" rel="noopener noreferrer">
+//               Click here to download output file
+//             </a>
+//           </>
+//         )}
+//         {error && (
+//           <Typography variant="body2" color="error" mt={2}>
+//             {error}
+//           </Typography>
+//         )}
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default CRO;
