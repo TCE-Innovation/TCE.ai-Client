d15cc7c5f72938a8f3268dac6430571c9b0d1510
matthewbayne <mbayne@tcelect.net>
2023-11-28 12:01:41 -0500

    trying again to deploy

--- src/components/PrivateFeatures/GenerateEmails.js
+++ src/components/PrivateFeatures/GenerateEmails.js
@@ -231,7 +231,6 @@ const GenerateEmails = () => {
                         </Grid>
                     </Grid>
                     </Box>
-
                     <Button
                         variant="contained"
                         color="primary"
