5346f6e998ca10384674b1c988e76f926717608a
matthewbayne <mbayne@tcelect.net>
2023-11-28 15:33:21 -0500

    added categories to tool menu private

--- src/components/Private/privateItems.js
+++ src/components/Private/privateItems.js
@@ -1,91 +1,147 @@
 //REACT
 import * as React from 'react';
-//import { useContext } from "react";
 
 //MUI
+import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+
+//ICONS
 import FeedIcon from '@mui/icons-material/Feed';
 import EmailIcon from '@mui/icons-material/Email';
-import SpokeIcon from '@mui/icons-material/Spoke';
-import ForumIcon from '@mui/icons-material/Forum';
 import DevicesOtherIcon from '@mui/icons-material/DevicesOther';
+import ForumIcon from '@mui/icons-material/Forum';
+import SpokeIcon from '@mui/icons-material/Spoke';
 import RailwayAlertIcon from '@mui/icons-material/RailwayAlert';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import WavingHandIcon from '@mui/icons-material/WavingHand';
+import InfoIcon from '@mui/icons-material/Info';
+import ConstructionIcon from '@mui/icons-material/Construction';
+import ApartmentIcon from '@mui/icons-material/Apartment';
 
-//conditional logic will live here, filter out features based on user role
-//import { AuthContext } from '../../authentication/Auth';
 
 const PrivateListItems = (props) => {
-    //const { userTitle } = useContext(AuthContext);
+    const [openInformation, setOpenInformation] = React.useState(false);
+    const [openTools, setOpenTools] = React.useState(false);
+    const [openOffice, setOpenOffice] = React.useState(false);
 
-    //set array of roles that can access each feature
-    // const emailRoles = ['Innovation Engineer', 'Innovation Manager', 'Innovation Director'];
-    // const croRoles = ['Innovation Engineer', 'Innovation Manager', 'Innovation Director'];
-    // const chatbotRoles = ['Innovation Engineer', 'Innovation Manager', 'Innovation Director'];
-    // const assetTrackerRoles = ['Innovation Engineer', 'Innovation Manager', 'Innovation Director'];
-    
-    // then check if user title is in array for each feature
+    const handleClickInformation = () => {
+        setOpenInformation(!openInformation);
+    };
+
+    const handleClickTools = () => {
+        setOpenTools(!openTools);
+    };
+
+    const handleClickOffice = () => {
+        setOpenOffice(!openOffice);
+    };
 
     return (
         <React.Fragment>
-            <ListItemButton onClick={() => props.onSelect('privateHome')}>
+            <ListItemButton onClick={handleClickInformation}>
                 <ListItemIcon>
-                    <WavingHandIcon />
+                    <InfoIcon />
                 </ListItemIcon>
-                <ListItemText primary="Welcome" />
+                <ListItemText primary="Information"  style={{ width: '150px' }}/>
+                    {openInformation ? <ExpandLess /> : <ExpandMore />}
             </ListItemButton>
 
-            <ListItemButton onClick={() => props.onSelect('tech')}>
+            <Collapse in={openInformation} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+
+                    <ListItemButton style={{ backgroundColor: '#f5f5f5' }} onClick={() => props.onSelect('privateHome')}>
+                        <ListItemIcon>
+                            <WavingHandIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Welcome"/>
+                    </ListItemButton>
+
+                    <ListItemButton style={{ backgroundColor: '#f5f5f5' }} onClick={() => props.onSelect('tech')}>
+                        <ListItemIcon>
+                            <FeedIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Tech Partners"/>
+                    </ListItemButton>
+
+                </List>
+            </Collapse>
+
+
+
+
+            <ListItemButton onClick={handleClickTools}>
                 <ListItemIcon>
-                    <FeedIcon />
+                    <ConstructionIcon />
                 </ListItemIcon>
-                <ListItemText primary="Tech Partners" />
+                <ListItemText primary="Tools" />
+                    {openTools ? <ExpandLess /> : <ExpandMore />}
             </ListItemButton>
 
-            <ListItemButton onClick={() => props.onSelect('generateEmails')}>
+            <Collapse in={openTools} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+
+                    <ListItemButton style={{ backgroundColor: '#f5f5f5' }} onClick={() => props.onSelect('generateEmails')}>
+                        <ListItemIcon>
+                            <EmailIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Generate an Email" />
+                    </ListItemButton>
+
+                    <ListItemButton style={{ backgroundColor: '#f5f5f5' }} onClick={() => props.onSelect('chatbot')}>
+                        <ListItemIcon>
+                            <ForumIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Chat Bot" />
+                    </ListItemButton>
+
+                    <ListItemButton style={{ backgroundColor: '#f5f5f5' }} onClick={() => props.onSelect('cro')}>
+                        <ListItemIcon>
+                            <SpokeIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Cable Run Optimizer" />
+                    </ListItemButton>
+
+                    <ListItemButton style={{ backgroundColor: '#f5f5f5' }} onClick={() => props.onSelect('go')}>
+                        <ListItemIcon>
+                            <RailwayAlertIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="GO Tracker" />
+                    </ListItemButton>
+
+                </List>
+            </Collapse>
+
+
+            <ListItemButton onClick={handleClickOffice}>
                 <ListItemIcon>
-                    <EmailIcon />
+                    <ApartmentIcon />
                 </ListItemIcon>
-                <ListItemText primary="Generate an Email" />
+                <ListItemText primary="Office" />
+                    {openOffice ? <ExpandLess /> : <ExpandMore />}
             </ListItemButton>
 
-            <ListItemButton onClick={() => props.onSelect('assetTracker')}>
-                <ListItemIcon>
-                    <DevicesOtherIcon />
-                </ListItemIcon>
-                <ListItemText primary="Equipment Checkout" />
-            </ListItemButton>
+            <Collapse in={openOffice} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    <ListItemButton style={{ backgroundColor: '#f5f5f5' }} onClick={() => props.onSelect('assetTracker')}>
+                        <ListItemIcon>
+                            <DevicesOtherIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Equipment Checkout" />
+                    </ListItemButton>
 
-            <ListItemButton onClick={() => props.onSelect('chatbot')}>
-                <ListItemIcon>
-                    <ForumIcon />
-                </ListItemIcon>
-                <ListItemText primary="Chat Bot" />
-            </ListItemButton>
-
-            <ListItemButton onClick={() => props.onSelect('cro')}>
-                <ListItemIcon>
-                    <SpokeIcon />
-                </ListItemIcon>
-                <ListItemText primary="Cable Run Optimizer" />
-            </ListItemButton>
-
-            <ListItemButton onClick={() => props.onSelect('go')}>
-                <ListItemIcon>
-                    <RailwayAlertIcon />
-                </ListItemIcon>
-                <ListItemText primary="GO Tracker" />
-            </ListItemButton>
-
-            <ListItemButton onClick={() => props.onSelect('overtime')}>
-                <ListItemIcon>
-                    <AccessTimeIcon />
-                </ListItemIcon>
-                <ListItemText primary="Overtime Tracker" />
-            </ListItemButton>
+                    <ListItemButton style={{ backgroundColor: '#f5f5f5' }} onClick={() => props.onSelect('overtime')}>
+                        <ListItemIcon>
+                            <AccessTimeIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Overtime Tracker" />
+                    </ListItemButton>
+                </List>
+            </Collapse>
 
         </React.Fragment>
     )
