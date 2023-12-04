fd1133aafff096b2a51a61b96fbd3fd74b861c0f
matthewbayne <mbayne@tcelect.net>
2023-12-04 16:22:06 -0500

    new slogan

--- src/components/PublicFeatures/AboutUs.js
+++ src/components/PublicFeatures/AboutUs.js
@@ -13,15 +13,14 @@ const AboutUs = () => {
         <div className="about-us-body">
           <div className="big-text">
             <p>
-              The TCE Innovation Group is a multidisciplinary team of engineers, data scientists, and designers passionate 
-              about the power of technology to transform the construction industry. 
+              Our mission is simple: <br />to find a better way.
             </p>
           </div>
-          <div className="about-us-header">[PIC]</div>
         </div>
           <div className="small-text">
             <p>
-              Serving New York City's premier infrastructure 
+            The TCE Innovation Group is a multidisciplinary team of engineers, data scientists, and designers passionate 
+              about the power of technology to transform the construction industry. Serving New York City's premier infrastructure 
               contractor, we are committed to leveraging technology to help our teams deliver successful projects in the world's most 
               challenging and complex construction environment. We are experienced in the design, development, and implementation of 
               software and hardware solutions, focusing our efforts across the business from project management and field execution 
