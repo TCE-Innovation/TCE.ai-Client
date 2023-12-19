4114454d61469e2be42a7acfce6de87e9d3a3b58
matthewbayne <mbayne@tcelect.net>
2023-12-19 16:02:02 -0500

    removed link from private techpartners image click

--- src/components/PrivateFeatures/TechPartners/TechPartners.js
+++ src/components/PrivateFeatures/TechPartners/TechPartners.js
@@ -24,7 +24,6 @@ const partnerData = [
     extraText: "Airtable assists various user groups and their specific needs, including TCIG, Estimating, Safety, Engineers, and Project Managers. TCIG and Estimating leverage Airtable's static database functionality to track internal assets and plan future bids, complementing their workflow with Bridgit Bench. The efficiency gains are further enhanced through Airtable's process logic and email automation, seamlessly integrated into multiple applications. The safety team benefits from automation in capturing track/safety certifications in a database and notifies safety managers of expiration dates, reducing downtime required for updating certifications which often have lead times of over three months.",
     contactName: "Chris Kruger",
     contactEmail: "ckruger@tcelect.net",
-    website: 'https://www.airtable.com',
     documents: [
       {title: "GO Tracker White Paper", url: "https://judlauent.sharepoint.com/:b:/s/TCEInnovation/ETEIqNwZPvFPsCP6KLnLxYIBjt_3LmFY_SAtRvYhBchi_Q?e=GgEatL"},
       {title: "GO Tracker SOP", url: "https://judlauent.sharepoint.com/:b:/s/TCEInnovation/EehYuVJdG9NJj_8Hp4_gqskBg8G5xFP0W6s7iQKaslYRDA?e=LL6t1E"}
@@ -37,7 +36,6 @@ const partnerData = [
     extraText: "Procore streamlines project management by centralizing document storage and eliminating the hassles of fragmented version control. This all-in-one platform enhances the workflows of all project personnel, whether they're in the field or in the office. Preconstruction tasks like estimate management, bid coordination, budget calculations, and team personnel tracking become effortless. During active projects, Procore excels at automating RFIs and Submittals, simplifying mark-ups and drawing labeling, maintaining comprehensive daily logs in real-time, providing fillable Quality and Safety reports, and offering numerous other invaluable features.",
     contactName: "Patrick Besser",
     contactEmail: "pbesser@tcelect.net",
-    website: 'https://www.procore.com',
     documents: []
   },
   {
@@ -47,7 +45,6 @@ const partnerData = [
     extraText: "OpenSpace offers engineers and project managers a practical solution for progress tracking, featuring a 360-degree camera attached to a hard hat that automatically records construction sites in 3-D. Executive Leadership can now remotely view the walk through of a project, in a similar manner to Google Maps, without the need to be there in-person. It allows for the addition of detailed photos and field notes during site walkthroughs, providing a comprehensive view for remote oversight, timeline tracking, and cost-effective insurance management.",
     contactName: "Rory O'Neill",
     contactEmail: "roneill@tcelect.net",
-    website: 'https://www.openspace.ai',
     documents: [
       {title: "OpenSpace SOP", url: "https://judlauent.sharepoint.com/:b:/s/TCEInnovation/ESGFy6NJP6pHteZx0vyLLygBb7XFxuGv4EnzGyuA5WOOQQ?e=GZQcte"},
     ]
@@ -59,7 +56,6 @@ const partnerData = [
     extraText: "Bridgit Bench improves project management by seamlessly integrating project and individual employee data, enabling more precise timelines and workforce optimization. During the pre-construction phase, it facilitates the creation and assignment of role titles and streamlines the coordination of Key Personnel for Design-Build Projects. As projects transition into the active phase and employees are assigned to specific roles, Bridgit Bench ensures optimal allocation, preventing overwork, and strategically placing employees based on their qualifications, certifications, and project history to meet both their needs and the project's requirements.",
     contactName: "Jacob Shavel",
     contactEmail: "jshavel@tcelect.net",
-    website: 'https://www.gobridgit.com',
     documents: [
       {title: "Bridgit White Paper", url: "https://judlauent.sharepoint.com/:b:/s/TCEInnovation/EQyO1dDTWFxDjvk9Gr5tN5YBQqF6b73gNlDd3jGcNjQ9Yg?e=Zp6Ply"},
     ]
@@ -71,7 +67,6 @@ const partnerData = [
     extraText: "EZOffice Inventory provides comprehensive asset tracking capabilities for items that can be allocated to various project sites. It mitigates the risk of lost or misused assets while optimizing resource utilization. Warehouse users benefit from streamlined processes for efficiently managing the inflow and outflow of items. Notable features, including location tracking and customized item groupings, eliminate the need for manual searches, ensuring zero downtime and efficient access to available in-stock items.",
     contactName: "Chris Kruger",
     contactEmail: "ckruger@tcelect.net",
-    website: 'https://ezo.io/ezofficeinventory/',
     documents: [
       {title: "TCE EZOffice Dashboard", url: "https://tcelectric.ezofficeinventory.com/dashboard"},
     ]
@@ -83,7 +78,6 @@ const partnerData = [
     extraText: "By utilizing Matterport cameras in construction settings, the camera seamlessly converts physical environments into immersive 3D models. This digital recreation enables remote viewing from any location and allows for quick, accurate distance measurements. Whether applied indoors or outdoors, Matterport's 3D documentation system captures a visual snapshot at each project milestone, serving as a comprehensive record-keeping tool throughout the construction process.",
     contactName: "Rory O'Neill",
     contactEmail: "roneill@tcelect.net",
-    website: 'https://matterport.com/',
     documents: []
   },
   {
@@ -93,7 +87,6 @@ const partnerData = [
     extraText: "Oracle P6 offers unparalleled control over project complexity by facilitating planning, scheduling, and resource management across your entire portfolio. P6 is tailored for large-scale, highly sophisticated project management and enables organizations to meet deadlines, stay within budget, and achieve their project goals. It excels in multi-user and multi-project environments with features like Gantt charts, Work Breakdown Structures, and comprehensive reporting tools.",
     contactName: "Sahul Kadarpeta",
     contactEmail: "skadarpeta@tcelect.net",
-    website: "https://www.oracle.com/construction-engineering/primavera-p6/",
     documents: []
   },
   {
@@ -103,7 +96,6 @@ const partnerData = [
     extraText: "Fieldwire streamlines collaboration and version control for multiple engineers, eliminating the need to constantly ensure that work is done on the latest document version. In the past, projects faced challenges with communication among engineers and maintaining consistency in project drawings. However, Fieldwire offers a standardized space for document history and collaborative drawing markups, effectively reducing the pain points associated with maintaining consistency, enhancing communication, and improving overall efficiency.",
     contactName: "Rory O'Neill",
     contactEmail: "roneill@tcelect.net",
-    website: 'https://www.fieldwire.com',
     documents: []
   }
 ];
@@ -113,11 +105,6 @@ const TechPartners = () => {
 
   const toggleReadMore = (index) => {
     setExpandedIndex(expandedIndex === index ? -1 : index);
-  };
-
-  const handleImageClick = (e, partner) => {
-    e.stopPropagation();
-    window.open(partner.website, '_blank');
   };
 
   const handleInsideCardClick = (e) => {
@@ -139,7 +126,7 @@ const TechPartners = () => {
           <div className={styles.card} key={index} onClick={() => toggleReadMore(index)}>
             
             <div className={styles.oneLinerContainer}>
-              <div className={styles.cardImage} onClick={(e) => handleImageClick(e, partner)}>
+              <div className={styles.cardImage}>
                 <img
                   src={partner.imgSrc}
                   alt={partner.name}
