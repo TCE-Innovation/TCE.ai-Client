bc75f37c51fe09abf83a888dd825031cfcc96f3f
matthewbayne <mbayne@tcelect.net>
2023-12-21 09:18:18 -0500

    redirect

--- src/components/Private/PrivateHome.js
+++ src/components/Private/PrivateHome.js
@@ -1,19 +1,16 @@
 //REACT
 import React from 'react';
 
-//MUI
-import { Container} from '@mui/material';
-
 //COMPONENTS
 import ContactUs from '../PrivateFeatures/ContactUs';
 import Welcome from '../PrivateFeatures/Welcome';
 
 const PrivateHome = () => {
     return (
-        <Container>
+        <>
             <Welcome/>
             <ContactUs/>
-        </Container>
+        </>
     );
 };
 
