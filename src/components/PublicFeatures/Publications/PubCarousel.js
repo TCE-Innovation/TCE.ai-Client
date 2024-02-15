7bf2201428accf4faed9b7c36c958b7c00e83eb7
matthewbayne <mbayne@tcelect.net>
2024-02-15 16:17:25 -0500

    revert variable width, messes up sliding

--- src/components/PublicFeatures/Publications/PubCarousel.js
+++ src/components/PublicFeatures/Publications/PubCarousel.js
@@ -16,10 +16,10 @@ export default function PubCarousel() {
         speed: 500,
         slidesToShow: 2, 
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 5000,
         arrows: false, 
-        variableWidth: true,
+
     };
 
     const data = [
