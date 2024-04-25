58d63d6f8f0e0b6288096cedfb1282ecb3754e31
matthewbayne <mbayne@tcelect.net>
2024-04-25 14:09:17 -0400

    updated public user menu a bit

--- src/components/Public/PublicNavigation/PublicNavigation.js
+++ src/components/Public/PublicNavigation/PublicNavigation.js
@@ -1,39 +1,32 @@
-//REACT
-import * as React from 'react';
-import {useContext, useState, useEffect} from "react";
+import React, { useContext, useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import {
+  AppBar, Box, Toolbar, Tooltip, IconButton, Typography, Menu, MenuItem, 
+  ListItemIcon, Avatar, Button
+} from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
-//MUI
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Tooltip from '@mui/material/Tooltip';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Avatar from '@mui/material/Avatar';
-import MenuItem from '@mui/material/MenuItem';
-import Button from "@mui/material/Button";
+import logo from '../../../img/Utils/logo.png';
+import whiteLogo from '../../../img/Utils/whiteLogo.png';
+import noUser from '../../../img/Utils/noUser.webp';
 
-//IMAGES
-import logo from '../../../img/Utils/logo.png'
-import whiteLogo from '../../../img/Utils/whiteLogo.png'
-import noUser from '../../../img/Utils/noUser.webp'
-
-//CONTEXTS
-import {AuthContext} from "../../../authentication/Auth";
+import { AuthContext } from "../../../authentication/Auth";
 import { useMsal } from "@azure/msal-react";
-
-//HOOKS
 import {useMicrosoftSignOut} from "../../Account/LogOut/LogOutFunc";
 import {useMicrosoftSignIn} from "../../Account/LoginFunc";
 
 function ResponsiveAppBar() {
-
-    //contexts
+    // Contexts
     const { userPic } = useContext(AuthContext);
+    const { accounts } = useMsal();
+    const isAuthenticated = accounts.length > 0;
 
-    //states
+    // Hooks for sign in and sign out
+    const MicrosoftSignOut = useMicrosoftSignOut();
+    const MicrosoftSignIn = useMicrosoftSignIn();
+
+    // States
     const [anchorElUser, setAnchorElUser] = useState(null);
     const [currentLogo, setCurrentLogo] = useState(logo);
     const [loginColor, setLoginColor] = useState({ textColor: 'white', borderColor: 'white', backgroundColor: 'none' });
@@ -46,12 +39,7 @@ function ResponsiveAppBar() {
                 behavior: 'smooth'
             });
         }
-    };    
-
-    const { accounts } = useMsal();
-
-    const isAuthenticated = accounts.length > 0;
-    const accSettings = ["Toolbox", "Log Out"]
+    };
 
     const handleOpenUserMenu = (event) => {
         setAnchorElUser(event.currentTarget);
@@ -61,125 +49,82 @@ function ResponsiveAppBar() {
         setAnchorElUser(null);
     };
 
-    const MicrosoftSignOut = useMicrosoftSignOut();
-    const MicrosoftSignIn = useMicrosoftSignIn();
-
-    //useEffect to change the logo and toolbox colors after scroll
+    // Handle scroll to adjust logo
     useEffect(() => {
         const mainContainer = document.getElementById('main-container');
-    
         const handleScroll = () => {
             const scrollPosition = mainContainer.scrollTop;
             const viewportHeight = window.innerHeight;
-            
-            //second page (where it turns white)
+
             if (scrollPosition > viewportHeight) { 
-                setCurrentLogo(whiteLogo); 
-                setLoginColor({ textColor: 'white', borderColor: 'white'});
-            } 
-            //first page (where it turns blue)
-            else {
-                setCurrentLogo(logo); 
-                setLoginColor({ textColor: '#1b365f'});
+                setCurrentLogo(whiteLogo);
+                setLoginColor({ textColor: 'white', borderColor: 'white' });
+            } else {
+                setCurrentLogo(logo);
+                setLoginColor({ textColor: '#1b365f' });
             }
         };
-    
+
         mainContainer.addEventListener('scroll', handleScroll);
         return () => mainContainer.removeEventListener('scroll', handleScroll);
-    
     }, []);
-    
+
+    const accSettings = [
+        { label: "Toolbox", icon: <HomeIcon />, link: "/private/welcome" },
+        { label: "Log Out", icon: <ExitToAppIcon />, action: () => MicrosoftSignOut() }
+    ];
+
     return (
         <>
             <AppBar position="fixed" elevation={0} sx={{ background: 'none', height: '3rem' }}>
                 <Toolbar sx={{ width: '100%' }} disableGutters>
                     <Box sx={{ display: 'flex', alignItems: 'center' }}>       
                         <div onClick={handleLogoClick}>
-                                <img
-                                    src={currentLogo}
-                                    alt='logo'
-                                    style={{
-                                        width: "10rem", 
-                                        marginLeft: "2em",
-                                        marginTop: "3em",
-                                    }}
-                                    draggable="false"
-                                />
-                            </div>
+                            <img src={currentLogo} alt='logo' style={{ width: "10rem", marginLeft: "2em", marginTop: "3em" }} draggable="false" />
+                        </div>
                     </Box>
                     <Box sx={{
                         display: 'flex',
                         justifyContent: 'flex-end',
-                        alignItems: 'right',
+                        alignItems: 'center',
                         marginTop: '3em',
                         marginRight: '2em',
-                        width: '100%'
-                    }}>           
-                            {isAuthenticated ? (
-                                <Box sx={{ display: 'flex', alignItems: 'center'}}>    
-                                    <IconButton onClick={handleOpenUserMenu}>
-                                        <Tooltip title="User Menu">
-                                            <Avatar
-                                                alt="You"
-                                                src={userPic ? userPic : noUser}
-                                                imgProps={{ referrerPolicy: "no-referrer" }}
-                                                style={{ width: '2.5em', height: '2.5em' }}
-                                            />
-                                        </Tooltip>
-                                    </IconButton>
-                                </Box>
-                            ) : (
-                                <Box sx={{display: "flex", flexDirection: "row"}}>
-                                    <Button
-                                        variant="outlined"
-                                        fullWidth
-                                        size="large"
-                                        onClick={MicrosoftSignIn}
-                                        sx={{ 
-                                            color: loginColor.textColor, 
-                                            borderColor: loginColor.borderColor, 
-                                            backgroundColor: loginColor.backgroundColor, 
-                                            fontWeight: 500, 
-                                            borderRadius: '1em', 
-                                            width: '12em', 
-                                            height: '2em',
-                                            borderWidth: '0.1em',
-                                            whiteSpace: 'nowrap',
-                                            '&:hover': { borderWidth: '0.1em', fontWeight: 700, color: '#003eab', borderColor: '#003eab' }, 
-                                        }}
-                                    >
-                                        Sign In
-                                    </Button>
-                                </Box>
-                            )}             
+                        width: '100%',
+                    }}>
+                        {isAuthenticated ? (
+                            <Box sx={{ display: 'flex', alignItems: 'center' }}>    
+                                <IconButton onClick={handleOpenUserMenu}>
+                                    <Tooltip title="User Menu">
+                                        <Avatar alt="You" src={userPic || noUser} style={{ width: '2.5em', height: '2.5em' }} />
+                                    </Tooltip>
+                                </IconButton>
+                            </Box>
+                        ) : (
+                            <Button onClick={MicrosoftSignIn} sx={{ ...loginColor, '&:hover': { color: '#003eab', borderColor: '#003eab' } }}>
+                                Sign In
+                            </Button>
+                        )}
                         <Menu
-                            sx={{ mt: '5rem' }}
-                            id="menu-appbar"
                             anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
                             open={Boolean(anchorElUser)}
                             onClose={handleCloseUserMenu}
+                            sx={{
+                                '& .MuiPaper-root': {
+                                    transform: 'translateX(-30px) !important', 
+                                }
+                            }}
                         >
-    
                             {accSettings.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                    {setting === "Toolbox" && (
-                                        <NavLink to="/private/welcome" style={{color: 'black', textDecoration: 'none'}}>
-                                            <Typography textAlign="center">{setting}</Typography>
+                                <MenuItem key={setting.label} onClick={handleCloseUserMenu}>
+                                    <ListItemIcon>{setting.icon}</ListItemIcon>
+                                    {setting.link ? (
+                                        <NavLink to={setting.link} style={{ color: '#1b365f', textDecoration: 'none' }}>
+                                            <Typography textAlign="center">{setting.label}</Typography>
                                         </NavLink>
-                                    )}
-                                    {setting === "Log Out" && (
-                                        <NavLink to="/" style={{color: 'black', textDecoration: 'none'}} onClick={MicrosoftSignOut} >
-                                            <Typography textAlign="center">{setting}</Typography>
-                                        </NavLink>
+                                    ) : (
+                                        <Typography textAlign="center" style={{ color: '#1b365f' }} onClick={setting.action}>
+                                            {setting.label}
+                                        </Typography>
                                     )}
                                 </MenuItem>
                             ))}
@@ -187,10 +132,9 @@ function ResponsiveAppBar() {
                     </Box>
                 </Toolbar>
             </AppBar>
-        <Toolbar />
+            <Toolbar />
         </>
     );
-    
 }
 
 export default ResponsiveAppBar;
