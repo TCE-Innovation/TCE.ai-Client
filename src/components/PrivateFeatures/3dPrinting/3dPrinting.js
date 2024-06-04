0a1b4be0dd1077d3ff78216e45f049412958724a
Rory O'Neill <roneill@tcelect.net>
2024-06-04 11:22:49 -0400

    Project drop down menu established

--- src/components/PrivateFeatures/3dPrinting/3dPrinting.js
+++ src/components/PrivateFeatures/3dPrinting/3dPrinting.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { FormControl, TextField, Button, Box, Select, InputLabel, CircularProgress } from '@mui/material';
+import React, { useState, useContext, useEffect } from 'react';
+import { AuthContext } from "../../../authentication/Auth";
+import { FormControl, TextField, Button, Box, Select, InputLabel, MenuItem, ListSubheader, CircularProgress } from '@mui/material';
 // import { getProjects } from '../../data/api'; // Assuming you have an API function to fetch projects
 // import style from './simpleForm.module.css'; // Assuming you have some CSS for styling
 import Typography from '@mui/material/Typography';
 import style from './3dPrinting.module.css';
 import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-
+import { sendAssetFormData, getActiveProjects } from '../../../data/Airtable';
 
 import ZBracketCuraImage from '../../../img/Request3DPrintingImages/z_bracket_cura.png';
 import ZBracketRealImage from '../../../img/Request3DPrintingImages/z_bracket_real.png';
@@ -19,22 +20,31 @@ const PrintingRequest = () => {
     const [isLoading, setIsLoading] = useState(false); 
     const [subject, setSubject] = useState('');
     const [dateNeeded, setDateNeeded] = useState(null);
+    const [projectOptions, setProjectOptions] = useState({ Active: [], Bidding: [] });
     const [project, setProject] = useState('');
     // const [projectOptions, setProjectOptions] = useState([]);
+
+    // useContext for email
+    const { userName, userEmail, userProjects } = useContext(AuthContext);
 
     // Asynchronously fetch project options when the component mounts
     useEffect(() => {
         const fetchProjectOptions = async () => {
             try {
-                // const options = await getProjects();
-                // setProjectOptions(options);
+                const options = await getActiveProjects();
+                // Group projects by status
+                const groupedOptions = {
+                    Active: [userProjects],
+                    Bidding: options.filter(o => o.status === 'Bidding'),
+                };
+                setProjectOptions(groupedOptions);
             } catch (error) {
                 console.error('Error fetching project options:', error);
             }
         };
 
         fetchProjectOptions();
-    }, []);
+    }, [userProjects]);
 
     // HANDLER FUNCTIONS
     const handleSubjectInputChange = (event) => {
@@ -68,16 +78,15 @@ const PrintingRequest = () => {
     return (
         <>
 
+    {/* OPENING TEXT */}
     <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" mb={3}>
             <div className={style.formDescription}>
                 If you have long lead equipment that you would like to get a 3D printed model of, please submit this form and Rory will reach out to confirm your request and coordinate handoff.
                 3D prints are limited to an area of 17.7 x 15.7 x 15.7 inches.
             </div>
-
-
     </Box>
 
-
+    {/* IMAGES */}
     <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" mb={3}>
         <Box display="flex" flexDirection="column" alignItems="center" mx={1}>
             <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
@@ -101,87 +110,94 @@ const PrintingRequest = () => {
     </Box>
 
 
+    {/* FORM */}
+    <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
 
-        <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
-
-            
-            {isLoading ? (
-                <CircularProgress style={{ display: 'block', margin: '0 auto' }} />
-            ) : !isSubmitted ? (
-                <div className="form-container">
-                    <Box display="flex" flexDirection="column" alignItems="center">
-                        
-                        <FormControl style={{ margin: "8px", width: "70%", marginBottom: "20px" }}>
-                            <InputLabel id="project-label">Project</InputLabel>
-                            <Select
-                                labelId="project-label"
-                                id="project"
-                                value={project}
-                                onChange={handleProjectInputChange}
-                                label="Project"
-                                required
+        
+        {isLoading ? (
+            <CircularProgress style={{ display: 'block', margin: '0 auto' }} />
+        ) : !isSubmitted ? (
+            <div className="form-container">
+                <Box display="flex" flexDirection="column" alignItems="center">
+                    
+                    <FormControl style={{ margin: "8px", width: "70%", marginBottom: "20px" }}>
+                        <InputLabel id="project-label">Project</InputLabel>
+                        <Select
+                            labelId="project-label"
+                            id="project"
+                            value={project}
+                            onChange={handleProjectInputChange}
+                            label="Project"
+                            required
                             >
-                                {/* {projectOptions.map((option, index) => (
-                                    <MenuItem key={index} value={option.name}>{option.name}</MenuItem>
-                                ))} */}
-                            </Select>
-                        </FormControl>
+                            <MenuItem value="Non-Project - 1010">Non-Project - 1010</MenuItem>
+                            <ListSubheader>My Active Jobs</ListSubheader>
+                            {Array.isArray(projectOptions.Active) && projectOptions.Active.map((option, index) => (
+                                <MenuItem key={index} value={option}>{option}</MenuItem>
+                            ))}
+                            <ListSubheader>Pursuits</ListSubheader>
+                            {Array.isArray(projectOptions.Bidding) && projectOptions.Bidding.map((option, index) => (
+                                <MenuItem key={index} value={option.name}>{option.name}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
 
-                        <TextField
-                            id="subject"
-                            label="Description of item to 3D print"
-                            value={subject}
-                            onChange={handleSubjectInputChange}
-                            style={{ margin: "8px", width: "70%", marginBottom: "20px" }}
-                        />
+                    <TextField
+                        id="subject"
+                        label="Description of item to 3D print"
+                        value={subject}
+                        onChange={handleSubjectInputChange}
+                        style={{ margin: "8px", width: "70%", marginBottom: "20px" }}
+                    />
 
-                        <TextField
-                            id="subject"
-                            label="Reason for 3D print request"
-                            value={subject}
-                            onChange={handleSubjectInputChange}
-                            style={{ margin: "8px", width: "70%", marginBottom: "20px" }}
-                        />
-                        <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                <DatePicker
-                                    label="Date Needed"
-                                    sx=  {{width: "70%", marginBottom: "28px", marginLeft: "0px", margin: "8px"}}
-                                    value={dateNeeded}
-                                    onChange={setDateNeeded}
-                                    renderInput={(params) => <TextField {...params} sx={{ }} />}
-                                />
-                        </LocalizationProvider>
-                        <Button
-                            onClick={handleSubmit}
-                            variant="contained"
-                            color="primary"
-                            style={{ width: "50%", marginTop: "20px" }}
-                            disabled={isButtonDisabled}
-                        >
-                            Submit
-                        </Button>
-                    </Box>
-                </div>
-            ) : (
-                <div className="form-container" style={{ textAlign: "center", color: "#1b365f" }}>
-                    <div>Thank you for your submission.</div>
+                    <TextField
+                        id="subject"
+                        label="Reason for 3D print request"
+                        value={subject}
+                        onChange={handleSubjectInputChange}
+                        style={{ margin: "8px", width: "70%", marginBottom: "20px" }}
+                    />
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <DatePicker
+                                label="Date Needed"
+                                sx=  {{width: "70%", marginBottom: "28px", marginLeft: "0px", margin: "8px"}}
+                                value={dateNeeded}
+                                onChange={setDateNeeded}
+                                renderInput={(params) => <TextField {...params} sx={{ }} />}
+                            />
+                    </LocalizationProvider>
                     <Button
-                        onClick={handleNewSubmission}
-                        variant="outlined"
-                        style={{ 
-                            width: "50%", 
-                            marginTop: "20px",
-                            color: "#1b365f",
-                            borderColor: "#1b365f",
-                            fontWeight: "500",
-                        }}
+                        onClick={handleSubmit}
+                        variant="contained"
+                        color="primary"
+                        style={{ width: "50%", marginTop: "20px" }}
+                        disabled={isButtonDisabled}
                     >
-                        Submit another request
+                        Submit
                     </Button>
-                </div>
-            )}
-        </div>
-        </>
+                </Box>
+            </div>
+        ) : (
+            <div className="form-container" style={{ textAlign: "center", color: "#1b365f" }}>
+                <div>Thank you for your submission.</div>
+                <Button
+                    onClick={handleNewSubmission}
+                    variant="outlined"
+                    style={{ 
+                        width: "50%", 
+                        marginTop: "20px",
+                        color: "#1b365f",
+                        borderColor: "#1b365f",
+                        fontWeight: "500",
+                    }}
+                >
+                    Submit another request
+                </Button>
+            </div>
+        )}
+    </div>
+
+    </>
     );
 };
 export default PrintingRequest;
