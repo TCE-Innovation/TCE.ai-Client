0f18c8dd254b056bd42ec9225ed6892186dc10a6
Steven DeFalco <sdefalco@tcelect.net>
2024-06-13 13:58:07 -0400

    removed unused lists, added Steven DeFalco as admin

--- src/admin/lists.js
+++ src/admin/lists.js
@@ -1,27 +1,10 @@
-//CHAT BOT
-const allProjectsAccess = [
-    "mbayne@tcelect.net",
-    "jshavel@tcelect.net",
-    "cdawson@tcelect.net",
-    "hlim@tcelect.net",
-    "roneill@tcelect.net",
-    "pbesser@tcelect.net"
-];
-
-const projWithChatbot = [
-    "p4", 
-    "ert", 
-    "fulton", 
-    "psd"
-];
-
-
 // ADMINS
 const adminList = [
     'Matthew Bayne',
     'Jacob Shavel',
     'Patrick Besser',
     'Rory O\'Neill',
+    'Steven DeFalco'
 ]
 
 //TOOLS
@@ -31,11 +14,10 @@ const toolList = [
     'Cable Run Optimizer',
     'GO Tracker',
     'Tool Usage Statistics',
+    'Drone Captures',
 ]
 
 module.exports = {
-    allProjectsAccess,
-    projWithChatbot, 
     adminList, 
     toolList
 }
