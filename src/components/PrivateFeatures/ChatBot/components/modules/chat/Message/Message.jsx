d07529d598e730b1687593ea3a18f23082ea84e6
shovan shahi <shovanshahi0373@gmail.com>
2024-06-17 14:36:10 +0545

    init

--- src/components/PrivateFeatures/ChatBot/components/modules/chat/Message/Message.jsx
+++ src/components/PrivateFeatures/ChatBot/components/modules/chat/Message/Message.jsx
@@ -1,4 +1,4 @@
-import React, { isValidElement } from "react";
+import React, { isValidElement, useState } from "react";
 
 import botImg from "../../../../assets/images/bot.png";
 
@@ -7,17 +7,42 @@ import Feedback from "./Feedback";
 
 import { Markdown } from "../../../common";
 
+import useGlobal from "../../../../hooks/useGlobal";
+
 import Wrapper from "./style";
 
 const Message = ({ body, id, isAI, citations, showfeedbackbuttons = true }) => {
+  const [imgError, setImgError] = useState(false);
+
+  const { userProfileUrl } = useGlobal();
+
   const displayName = isAI ? "Chat" : "You";
+
+  const isUserImgError = !userProfileUrl || imgError;
+
   return (
     <Wrapper>
       <div
         className={`author-avatar ${!isAI ? "user-avatar" : ""}`}
         data-name={!isAI ? displayName[0].toUpperCase() : ""}
       >
-        {isAI ? <img alt={"bot"} src={botImg} /> : null}
+        {isAI ? (
+          <img alt={"bot"} src={botImg} />
+        ) : isUserImgError ? null : (
+          <img
+            src={userProfileUrl}
+            style={{
+              objectFit: "cover",
+              width: "100%",
+              height: "100%",
+              display: "block",
+            }}
+            alt={""}
+            onError={() => {
+              setImgError(true);
+            }}
+          />
+        )}
       </div>
       <div className="message-container">
         <div className="author">{displayName}</div>
