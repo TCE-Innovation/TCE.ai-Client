94f3b98216486b490d7b173e4c142d462a4223be
chowleyTCE <chowley@tcelect.net>
2024-08-26 10:55:07 -0400

    training component working with links hardcoded

--- src/components/PrivateFeatures/Training.js
+++ src/components/PrivateFeatures/Training.js
@@ -4,23 +4,35 @@ import { getTrainingLink } from '../../data/Airtable';
 import { FormControl, InputLabel, Select, MenuItem, Box, Typography } from '@mui/material';
 import { AuthContext } from "../../authentication/Auth";
 
+const toolMap = {
+    'Procore': ['Doc Control', 'Engineer', 'Quality Manager', 'Safety Engineer'],
+    'OpenSpace' : [],
+};
+
+const trainingIframeMap = {
+    'Doc Control': "https://scribehow.com/page-embed/Document_Controller_SOP_for_Using_Procore_Construction_Project_Management_Software__Cgsp0kyNTp6QRqg6OO9gSg",
+    'Engineer': "https://scribehow.com/page-embed/Engineer_SOP_for_Using_Procore_Construction_Project_Management_Software__3Rv5ubP7TqaxLF438PEN_A",
+    'Quality Manager': "https://scribehow.com/page-embed/Quality_Manager_SOP_for_Using_Procore_Construction_Project_Management_Software__XP9W64YmRaKoqnju_mk-fQ",
+    'Safety Engineer': "https://scribehow.com/page-embed/Safety_Engineer_SOP_for_Using_Procore_Construction_Project_Management_Software__wy6yH2eQSJapHpABDWa8Ug",
+};
+
 const TrainingPage = () => {
-    const [tools, setTools] = useState([]);
-    const [roles, setRoles] = useState([]);
     const [selectedTool, setSelectedTool] = useState('');
     const [selectedRole, setSelectedRole] = useState('');
+    const [roles, setRoles] = useState([]);
     const [iframeLoaded, setIframeLoaded] = useState(false);
-    const [trainingLink, setTrainingLink] = useState('');
-
-    const { userEmail } = useContext(AuthContext);
+    const [iframeLink, setIframeLink] = useState('');
+    const [filteredTools, setFilteredTools] = useState([]);
 
     useEffect(() => {
         const fetchTrainingLinks = async () => {
             try {
-                const data = await getTrainingLink(selectedTool);
-                console.log(data);
-                setTools(['Procore']);
-                setRoles(data);
+                //const data = await getTrainingLink(selectedTool);
+                const tools = Object.keys(toolMap);
+                setFilteredTools(tools);
+                if (tools.length > 0) {
+                    setSelectedTool(tools[0]);
+                }
             } catch (error) {
                 console.error('Error fetching training links:', error);
             }
@@ -28,18 +40,22 @@ const TrainingPage = () => {
         fetchTrainingLinks();
     }, []);
 
+    useEffect(() => {
+        setRoles(toolMap[selectedTool] || []);
+        setSelectedRole(toolMap[selectedTool]?.[0] || '');
+    }, [selectedTool]);
+
+
+    useEffect(() => {
+        setIframeLink(trainingIframeMap[selectedRole] || '');
+    }, [selectedRole]);
+
     const handleToolChange = (event) => {
-        const tool = 'Procore'
-        setSelectedTool(tool);
-        setRoles(Object.keys(tools[tool]));
-        setSelectedRole('');
-        setTrainingLink('');
+        setSelectedTool(event.target.value);    
     };
 
     const handleRoleChange = (event) => {
-        const role = event.target.value;
-        setSelectedRole(role);
-        setTrainingLink(tools[selectedTool][role]);
+        setSelectedRole(event.target.value);
     };
 
     const handleIframeLoad = () => {
@@ -63,12 +79,12 @@ const TrainingPage = () => {
                         onChange={handleToolChange}
                         label="Tool"
                     >
-                        {Object.keys(tools).map((tool) => (
+                        {filteredTools.map((tool) => (
                             <MenuItem key={tool} value={tool}>{tool}</MenuItem>
                         ))}
                     </Select>
                 </FormControl>
-                {selectedTool && (
+                {roles.length > 1 && (
                     <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
                         <InputLabel id="role-label">Role</InputLabel>
                         <Select
@@ -85,11 +101,11 @@ const TrainingPage = () => {
                     </FormControl>
                 )}
             </Box>
-            {trainingLink && (
+            {iframeLink && (
                 <div style={{ display: iframeLoaded ? 'block' : 'none', width: '100%', height: '75vh', margin: 'auto' }}>
                     <iframe
                         onLoad={handleIframeLoad}
-                        src={trainingLink}
+                        src={iframeLink}
                         style={{ width: '100%', height: '100%', border: '1px solid #ccc', background: 'transparent' }}
                         title="Training"
                     ></iframe>
