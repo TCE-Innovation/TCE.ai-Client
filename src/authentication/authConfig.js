d9e7376bd0c1a20df48b1d689645ae7f8982ed1a
chowleyTCE <chowley@tcelect.net>
2025-03-19 14:56:27 -0400

    forgot authconfig oops

--- src/authentication/authConfig.js
+++ src/authentication/authConfig.js
@@ -2,10 +2,10 @@ export const msalConfig = {
     auth: {
         clientId: '5948db4f-c992-4d98-b5ae-ca25d1010926',
         authority: 'https://login.microsoftonline.com/3539293e-58fa-4bab-a02e-18dc57fa9737',
-        redirectUri: "http://localhost:3000/private/welcome"
-        // redirectUri: (process.env.REACT_APP_ENV === 'dev' || process.env.NODE_ENV === 'development')
-        //     ? "https://dev.tcig.nyc/private/welcome"
-        //     : "https://tcig.nyc/private/welcome",
+        // redirectUri: "http://localhost:3000/private/welcome"
+        redirectUri: (process.env.REACT_APP_ENV === 'dev' || process.env.NODE_ENV === 'development')
+            ? "https://dev.tcig.nyc/private/welcome"
+            : "https://tcig.nyc/private/welcome",
     },
 };
 
