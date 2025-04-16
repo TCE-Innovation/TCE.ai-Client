9070b2a6cc98990ec37249e3422f6ec6b29491e8
Rory O'Neill <120664694+RoryMO@users.noreply.github.com>
2025-04-16 12:11:54 -0400

    authConfig to tce.tools

--- src/authentication/authConfig.js
+++ src/authentication/authConfig.js
@@ -3,8 +3,8 @@ export const msalConfig = {
         clientId: '5948db4f-c992-4d98-b5ae-ca25d1010926',
         authority: 'https://login.microsoftonline.com/3539293e-58fa-4bab-a02e-18dc57fa9737',
         redirectUri: (process.env.REACT_APP_ENV === 'dev' || process.env.NODE_ENV === 'development')
-            ? "https://dev.tcig.nyc/private/welcome"
-            : "https://tcig.nyc/private/welcome",
+            ? "https://dev.tce.tools/private/welcome"
+            : "https://tce.tools/private/welcome",
     },
 };
 
