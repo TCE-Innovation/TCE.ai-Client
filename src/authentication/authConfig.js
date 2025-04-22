69f49dea28d236677f4aef1866ca84a3b848a4c3
Rory O'Neill <roneill@tcelect.net>
2025-04-22 16:11:00 -0400

    new url set for prod

--- src/authentication/authConfig.js
+++ src/authentication/authConfig.js
@@ -6,8 +6,8 @@ export const msalConfig = {
         redirectUri: (process.env.REACT_APP_ENV === 'dev' || process.env.NODE_ENV === 'development')
             // ? "https://dev.tcig.nyc/private/welcome"
             ? "https://dev.tce.tools/private/welcome" // Updated dev url
-            : "https://tcig.nyc/private/welcome",  // OLd URL
-            // : "https://tce.tools/private/welcome",  // Updated URL
+            // : "https://tcig.nyc/private/welcome",  // OLd URL
+            : "https://tce.tools/private/welcome",  // Updated URL
     },
 };
 
