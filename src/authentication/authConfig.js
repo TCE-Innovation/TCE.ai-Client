4c4a33146b30ae485cc73931970c070b4bf8b64a
Steven DeFalco <sdefalco@tcelect.net>
2025-06-09 11:56:30 -0400

    Added local storage to cache and postLogoutRedirectUri

--- src/authentication/authConfig.js
+++ src/authentication/authConfig.js
@@ -2,12 +2,17 @@ export const msalConfig = {
     auth: {
         clientId: '5948db4f-c992-4d98-b5ae-ca25d1010926',
         authority: 'https://login.microsoftonline.com/3539293e-58fa-4bab-a02e-18dc57fa9737',
-        // redirectUri: "http://localhost:3000/private/welcome"
+        //redirectUri: "http://localhost:3000/private/welcome"
         redirectUri: (process.env.REACT_APP_ENV === 'dev' || process.env.NODE_ENV === 'development')
-            // ? "https://dev.tcig.nyc/private/welcome"
-            ? "https://dev.tce.tools/private/welcome" // Updated dev url
-            : "https://tcig.nyc/private/welcome",  // OLd URL
-            // : "https://tce.tools/private/welcome",  // Updated URL
+            ? "https://dev.tce.tools/private/welcome" 
+            : "https://tce.tools/private/welcome",
+        postLogoutRedirectUri: (process.env.REACT_APP_ENV === 'dev' || process.env.NODE_ENV === 'development')
+            ? "https://dev.tce.tools"
+            : "https://tce.tools",
+    },
+    cache: {
+        cacheLocation: "localStorage", 
+        storeAuthStateInCookie: true, 
     },
 };
 
