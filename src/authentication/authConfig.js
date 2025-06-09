2a08ab8dc1c6d4656685576318dd7156e8843ac3
Steven DeFalco <sdefalco@tcelect.net>
2025-06-09 10:57:49 -0400

    Adding more postLogoutRedirectUri and cache details

--- src/authentication/authConfig.js
+++ src/authentication/authConfig.js
@@ -1,10 +1,19 @@
+import { cache } from "react";
+
 export const msalConfig = {
     auth: {
         clientId: '5948db4f-c992-4d98-b5ae-ca25d1010926',
         authority: 'https://login.microsoftonline.com/3539293e-58fa-4bab-a02e-18dc57fa9737',
         redirectUri: (process.env.REACT_APP_ENV === 'dev' || process.env.NODE_ENV === 'development')
-            ? "https://dev.tce.tools/private/welcome"
-            : "https://tce.tools/private/welcome",
+            ? "https://dev.tce.tools/private/home"
+            : "https://tce.tools/private/home",
+        postLogoutRedirectUri: (process.env.REACT_APP_ENV === 'dev' || process.env.NODE_ENV === 'development')
+            ? "https://dev.tce.tools"
+            : "https://tce.tools",
+    },
+    cache: {
+        cacheLocation: "localStorage", 
+        storeAuthStateInCookie: true, 
     },
 };
 
