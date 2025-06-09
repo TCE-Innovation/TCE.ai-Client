41a21fa9b048188f4482f9a16a1c98e45270d917
Steven DeFalco <sdefalco@tcelect.net>
2025-06-09 14:16:12 -0400

    Updated redirectUri to reflect Authentication in Azure

--- src/authentication/authConfig.js
+++ src/authentication/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
         clientId: '5948db4f-c992-4d98-b5ae-ca25d1010926',
         authority: 'https://login.microsoftonline.com/3539293e-58fa-4bab-a02e-18dc57fa9737',
-        redirectUri: window.location.origin + "/private/home",
+        redirectUri: window.location.origin + "/private/welcome",
         postLogoutRedirectUri: window.location.origin,
     },
     cache: {
