5c8e943439f9948c4426fa21c5d415e41dbd9c37
Steven DeFalco <sdefalco@tcelect.net>
2025-06-09 11:31:49 -0400

    Reverting /private/welcome because it is specified in Entra Autentication

--- src/authentication/authConfig.js
+++ src/authentication/authConfig.js
@@ -1,12 +1,11 @@
-import { cache } from "react";
-
 export const msalConfig = {
     auth: {
         clientId: '5948db4f-c992-4d98-b5ae-ca25d1010926',
         authority: 'https://login.microsoftonline.com/3539293e-58fa-4bab-a02e-18dc57fa9737',
+        // redirectUri: "http://localhost:3000/private/welcome", 
         redirectUri: (process.env.REACT_APP_ENV === 'dev' || process.env.NODE_ENV === 'development')
-            ? "https://dev.tce.tools/private/home"
-            : "https://tce.tools/private/home",
+            ? "https://dev.tce.tools/private/welcome"
+            : "https://tce.tools/private/welcome",
         postLogoutRedirectUri: (process.env.REACT_APP_ENV === 'dev' || process.env.NODE_ENV === 'development')
             ? "https://dev.tce.tools"
             : "https://tce.tools",
