1f54a0af22b983e5da179deaaa5edb9c718b904f
Steven DeFalco <sdefalco@tcelect.net>
2025-06-12 11:29:39 -0400

    Added 'Full Screen' button to Monthly Executive Reports

--- src/components/PrivateFeatures/MonthlyExecutiveReports.js
+++ src/components/PrivateFeatures/MonthlyExecutiveReports.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect, useRef } from 'react';
 import TrainLoader from '../General/TrainLoader';
-import { getPBILog } from '../../data/Airtable'; // Adjust the path to where your function is located
+import { getPBILog } from '../../data/Airtable';
+import { Box } from '@mui/material';
 
 const MonthlyExecutiveReports = () => {
     const [iframeSrc, setIframeSrc] = useState('');
     const [iframeLoaded, setIframeLoaded] = useState(false);
     const iframeRef = useRef(null);
+
+    const handleFullScreen = () => {
+        if (iframeRef.current) {
+            if (iframeRef.current.requestFullscreen) {
+                iframeRef.current.requestFullscreen();
+            } else if (iframeRef.current.mozRequestFullScreen) { // Firefox
+                iframeRef.current.mozRequestFullScreen();
+            } else if (iframeRef.current.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+                iframeRef.current.webkitRequestFullscreen();
+            } else if (iframeRef.current.msRequestFullscreen) { // IE/Edge
+                iframeRef.current.msRequestFullscreen();
+            }
+        }
+    };
 
     useEffect(() => {
         const fetchLink = async () => {
@@ -30,14 +45,30 @@ const MonthlyExecutiveReports = () => {
     };
 
     return (
-        <div style={{ width: '100%', height: 'calc(100vh - 168px)', display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
+        <div style={{ width: '100%', display: 'flex', flexDirection: "column", alignContent: 'center', marginTop: 0 }}>
             {!iframeLoaded && (
                 <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 100 }}>
                     <TrainLoader />
                 </div>
             )}
+            <Box sx={{ width: '100%', display: 'flex', marginBottom: 2, height: '40px' }}>
+                <div style={{ position: 'relative', width: '100%' }}>
+                    <button 
+                        onClick={handleFullScreen} 
+                        style={{ 
+                            position: 'absolute', 
+                            top: 0, 
+                            right: 0, 
+                            padding: '8px 12px', 
+                            cursor: 'pointer',
+                            borderRadius: '4px',
+                        }}>
+                        Full Screen
+                    </button>
+                </div>
+            </Box>
             {iframeSrc && (
-                <div style={{ flex: 1, width: '100%', height: '100%', display: iframeLoaded ? 'block' : 'none', overflow: 'auto' }}>
+                <div style={{ display: iframeLoaded ? 'block' : 'none', width: '100%', height: '78vh', margin: 'auto' }}>
                     <iframe
                         ref={iframeRef}
                         onLoad={handleIframeLoad}
